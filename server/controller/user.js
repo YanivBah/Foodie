0e9bf5c42b5d7c6ab945c55516c6fc09ac709700
3agleFF67c1 <ybahalker@gmail.com>
2021-04-30 11:33:15 +0300

    initial files structure

---
+++ server/controller/user.js
@@ -0,0 +1,21 @@
+const User = require("../model/user");
+
+const loginUser = async (req,res) => {
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password);
+    const token = await user.generateAuthToken();
+    res.send({user, token});
+  } catch(e) {
+    res.status(400).send(e.message);
+  }
+};
+
+const signupUser = async (req,res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send(user);
+  } catch(e) {
+    res.status(400).send(e)
+  }
+};
---
+++ server/router/user.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = new express.Router();
+// const auth = require("../middleware/auth");
+// const { functions } = require("../controller/user");
+
+// router.get("/profile", auth, userDetails);
+
+module.exports = router;
