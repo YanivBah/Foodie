abcb0f86329d8902b3e4bad83087aed9e75781a1
3agleFF67c1 <ybahalker@gmail.com>
2021-04-30 13:13:49 +0300

    User can logout now

--- server/controller/user.js
+++ server/controller/user.js
@@ -20,4 +20,16 @@ const signupUser = async (req,res) => {
   }
 };
 
-module.exports = { loginUser, signupUser };
+const logoutUser = async (req,res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter(token => {
+      return token.token !== req.token
+    });
+    await req.user.save();
+    res.send({ message: "Logged out" });
+  } catch(e) {
+    res.status(400).send(e)
+  }
+}
+
+module.exports = { loginUser, signupUser, logoutUser };
--- server/router/user.js
+++ server/router/user.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = new express.Router();
-// const auth = require("../middleware/auth");
-const { loginUser, signupUser } = require("../controller/user");
+const auth = require("../middleware/auth");
+const { loginUser, signupUser, logoutUser } = require("../controller/user");
 
 router.post("/api/user/signup", signupUser);
 router.post("/api/user/login", loginUser);
+router.post("/api/user/logout",auth, logoutUser);
 
 module.exports = router;
